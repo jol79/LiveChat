c68920ba2cff4fa77d59383b0417136b5b086b1e
jol79 <jolhuck@gmail.com>
2021-12-25 21:51:53 +0200

    Structure of the application was changed, folder with a project was moved to the root folder

---
+++ src/MessageContext.js
@@ -0,0 +1,8 @@
+import { createContext, useContext } from "react";
+
+export const MessagesContext = React.createContext();
+const useMessagesContext = () => {
+    
+}
+
+// const [messages, setMessages] = useContext(MessagesContext);
